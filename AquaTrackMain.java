1de73e70926def4027132c320a2cc7be62643620
dodol <167850988+nikaAerox@users.noreply.github.com>
2026-01-14 21:06:37 +0800

    Update AquaTrackMain.java

--- AquaTrackMain.java
+++ AquaTrackMain.java
@@ -1,36 +1,73 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class AquaTrackMain{
 
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        uiPrinter uiP = new uiPrinter();
-        //addSample add = new addSample();
-        queuePending queue = new queuePending();
-        viewSample view = new viewSample();
-        System.out.print(uiP.menu()); //placeholder x pakai
-        int opt = input.nextInt();
-        boolean optEntered = false;
-        
-        //pandai2 buat ui
-        while (true) {
-            optEntered = true;
-            switch (opt){
-            case 1:
-                //add.addSample();
-                break;
-            case 2:
-                view.viewSample();
-                break;
-            
-            case 3:
-                queue.queuePending();
-                break;
-            
-            default:
-                System.out.println("Invalid input please try again....");
-                break;
+
+        int opt = -1;
+        Scanner in = new Scanner(System.in);
+        SampleLinkedList<WaterSample> normalList = new SampleLinkedList<>();
+        TaskQueue<WaterSample> riskQueue = new TaskQueue<>();
+
+
+    do{
+        System.out.print("Enter 1(add),2(view),3,0(exit): ");
+        opt = in.nextInt();
+            if(opt == 1){
+        // ---------- READ FILE & SEPARATE ----------
+        try (BufferedReader br = new BufferedReader(new FileReader("Samples.txt"))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                String id = data[0];
+                double temp = Double.parseDouble(data[1]);
+                double pH = Double.parseDouble(data[2]);
+                double ammonia = Double.parseDouble(data[3]);
+                double nitrite = Double.parseDouble(data[4]);
+                double nitrate = Double.parseDouble(data[5]);
+                double alkalinity = Double.parseDouble(data[6]);
+                double gh = Double.parseDouble(data[7]);
+                String date = data[9]; // risk is auto-calculated
+
+                WaterSample ws = new WaterSample(
+                        id, temp, pH, ammonia, nitrite,
+                        nitrate, alkalinity, gh, date
+                );
+
+                if (ws.getRiskLvl().equals("Normal")) {
+                    normalList.addLast(ws);
+                } else {
+                    riskQueue.enqueue(ws);
+                }
             }
-            
+
+        } catch (IOException e) {
+            System.out.println("Error reading Samples.txt");
+        }
+
+        // ---------- CALL WATER SAMPLE MENU ----------
+        WaterSample.wSample(normalList, riskQueue);
+    }
+
+    else if(opt == 2){
+
+            ArrayList<WaterSample> Samples = SampleDataLoader.loadFromFile("Samples.txt");
+
+            sampleDisplay.displayPaging(Samples);
+        
+    }
+    else if(opt == 3){
+
+    }
+
+
+
+
+    }while(opt != 0);
         }
     }
-}
