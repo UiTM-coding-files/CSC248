b4f57dbba9dcc8c399a3291a51d89fe550a9d5d8
tontowlow <deruu0207@gmail.com>
2026-01-26 21:06:54 +0800

    rollback main

--- AquaTrackMain.java
+++ AquaTrackMain.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -40,7 +44,7 @@ public class AquaTrackMain {
             }
 
             // ================= LOAD FILE =================
-            SampleDataLoader.loadSamples(normalList, riskQueue);
+            loadSamples(normalList, riskQueue);
 
             // ================= MENU =================
             do {
@@ -72,7 +76,7 @@ public class AquaTrackMain {
                         sampleDisplay.clearScreen();
                         normalList.clear();
                         riskQueue = new TaskQueue<>();
-                        SampleDataLoader.loadSamples(normalList, riskQueue);
+                        loadSamples(normalList, riskQueue);
 
                         queuePending qp = new queuePending();
                         qp.queuePending(normalList, riskQueue);
@@ -91,4 +95,52 @@ public class AquaTrackMain {
         }
     }
 
+    // ================= SAFE FILE LOADER =================
+    private static void loadSamples(SampleLinkedList<WaterSample> normalList,
+                                    TaskQueue<WaterSample> riskQueue) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("Samples.txt"))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(",");
+
+                // PREVENT ArrayIndexOutOfBounds
+                if (data.length < 10) {
+                    continue;
+                }
+
+                String id = data[0];
+                double temp = Double.parseDouble(data[1]);
+                double pH = Double.parseDouble(data[2]);
+                double ammonia = Double.parseDouble(data[3]);
+                double nitrite = Double.parseDouble(data[4]);
+                double nitrate = Double.parseDouble(data[5]);
+                double alkalinity = Double.parseDouble(data[6]);
+                double gh = Double.parseDouble(data[7]);
+                LocalDate date = LocalDate.parse(data[9]);
+
+                boolean actionTaken = false;
+                if (data.length > 10) {
+                    actionTaken = Boolean.parseBoolean(data[10]);
+                }
+
+                WaterSample ws = (data.length > 10)
+                        ? new WaterSample(id, temp, pH, ammonia, nitrite,
+                                nitrate, alkalinity, gh, date, actionTaken)
+                        : new WaterSample(id, temp, pH, ammonia, nitrite,
+                                nitrate, alkalinity, gh, date);
+
+                if (ws.getRiskLvl().equals("Normal")) {
+                    normalList.addLast(ws);
+                } else {
+                    riskQueue.enqueue(ws);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading Samples.txt");
+        }
+    }
 }
