2aa437df59de0851da51bf984bc3b0cbcede2919
Telbet <lbertethe@gmail.com>
2026-01-26 17:23:49 +0800

    Refactor SampleDataLoader for improved readability

--- SampleDataLoader.java
+++ SampleDataLoader.java
@@ -2,16 +2,13 @@ import java.io.*;
 import java.time.LocalDate;
 import java.util.*;
 
-
 public class SampleDataLoader {
 
-private static final String FILE_NAME = "Samples.txt";
-static final int sample_per_row = 4;
-static final int row_per_page = 2;
-    
- static ArrayList<WaterSample> loadFromFile(String Samples){
+    private static final String FILE_NAME = "Samples.txt";
+
+    static ArrayList<WaterSample> loadFromFile(String Samples) {
         ArrayList<WaterSample> sampleList = new ArrayList<>();
-        try(BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(",");
@@ -29,7 +26,8 @@ static final int row_per_page = 2;
                 if (data.length > 10) {
                     try {
                         actionTaken = Boolean.parseBoolean(data[10]);
-                    } catch (Exception ignored) {}
+                    } catch (Exception ignored) {
+                    }
                 }
 
                 WaterSample ws;
@@ -46,4 +44,51 @@ static final int row_per_page = 2;
         return sampleList;
     }
 
+    static void loadSamples(SampleLinkedList<WaterSample> normalList, TaskQueue<WaterSample> riskQueue) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("Samples.txt"))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(",");
+
+                // PREVENT ArrayIndexOutOfBounds
+                if (data.length < 10) {
+                    continue;
+                }
+
+                String id = data[0];
+                double temp = Double.parseDouble(data[1]);
+                double pH = Double.parseDouble(data[2]);
+                double ammonia = Double.parseDouble(data[3]);
+                double nitrite = Double.parseDouble(data[4]);
+                double nitrate = Double.parseDouble(data[5]);
+                double alkalinity = Double.parseDouble(data[6]);
+                double gh = Double.parseDouble(data[7]);
+                LocalDate date = LocalDate.parse(data[9]);
+
+                boolean actionTaken = false;
+                if (data.length > 10) {
+                    actionTaken = Boolean.parseBoolean(data[10]);
+                }
+
+                WaterSample ws = (data.length > 10)
+                        ? new WaterSample(id, temp, pH, ammonia, nitrite,
+                                nitrate, alkalinity, gh, date, actionTaken)
+                        : new WaterSample(id, temp, pH, ammonia, nitrite,
+                                nitrate, alkalinity, gh, date);
+
+                if (ws.getRiskLvl().equals("Normal")) {
+                    normalList.addLast(ws);
+                } else {
+                    riskQueue.enqueue(ws);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading Samples.txt");
+        }
+
+    }
 }
